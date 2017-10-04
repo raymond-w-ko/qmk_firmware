ff4c7c253648da1fdea878200cd32ffd6477409b
Raymond W. Ko <raymond.w.ko@gmail.com>
2017-10-03 20:48:13 -0400

    modifer keys location adjustment

--- keyboards/planck/keymaps/raymond/keymap.c
+++ keyboards/planck/keymaps/raymond/keymap.c
@@ -63,14 +63,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | GUI  | GUI  |Lower | Semi |Space |Raise | Left | Down |  Up  |Right |
+ * | GUI  |      |      | Alt  |Lower | Ctrl |Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SEMIMAP, KC_QUOT},
   {KC_LSPO, ZEMIMAP, KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC},
-  {KC_LCTL, KC_LALT, KC_LGUI, KC_LGUI, LOWER,   SEMIMAP, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LGUI, KC_LALT, KC_LGUI, KC_LALT, LOWER,   KC_LCTL, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Semimap */
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = {
   {_______, RESET,   DEBUG,   _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, WINCUNI,  OSXUNI,  LNXUNI,  WINUNI, _______},
+  {_______, _______, MU_MOD,  _______, _______, _______, _______, WINCUNI,  OSXUNI,  LNXUNI,  WINUNI, _______},
   {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 }
