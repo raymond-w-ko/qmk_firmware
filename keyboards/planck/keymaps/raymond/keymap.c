1628462da60d0c61f33c0cc702727d8e14c56785
Raymond W. Ko <raymond.w.ko@gmail.com>
2017-10-05 13:00:41 -0400

    too many collisions with Z, also add more modifiers

--- keyboards/planck/keymaps/raymond/keymap.c
+++ keyboards/planck/keymaps/raymond/keymap.c
@@ -63,14 +63,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | GUI  |      |      | Alt  |Lower | Ctrl |Space |Raise | Left | Down |  Up  |Right |
+ * | GUI  |Enter |  Del | Alt  |Lower | Ctrl |Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SEMIMAP, KC_QUOT},
-  {KC_LSPO, ZEMIMAP, KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC},
-  {KC_LGUI, KC_LALT, KC_LGUI, KC_LALT, LOWER,   KC_LCTL, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC},
+  {KC_LGUI, KC_ENT,  KC_DEL,  KC_LALT, LOWER,   KC_LCTL, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Semimap */
