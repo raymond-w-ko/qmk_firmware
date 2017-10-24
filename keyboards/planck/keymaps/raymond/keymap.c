ddf2b447b6c43eaa22546d77fa9ad022e7140d08
Raymond W. Ko <raymond.w.ko@gmail.com>
2017-10-24 09:55:39 -0400

    change keymaps to make backspace standalone

--- keyboards/planck/keymaps/raymond/keymap.c
+++ keyboards/planck/keymaps/raymond/keymap.c
@@ -83,10 +83,10 @@ const uint32_t PROGMEM unicode_map[] = {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [ALPH] = {
-{KC_Q,          KC_W,     KC_E,     KC_R,  KC_T,          KC_LBRC,        KC_RBRC, KC_Y,   KC_U,  KC_I,     KC_O,     KC_P},
-{KC_A,          KC_S,     KC_D,     KC_F,  KC_G,          KC_LPRN,        KC_RPRN, KC_H,   KC_J,  KC_K,     KC_L,     SEMIKEY},
-{SFT_T(KC_Z),   KC_X,     KC_C,     KC_V,  KC_B,          KC_LCBR,        KC_RCBR, KC_N,   KC_M,  KC_COMM,  KC_DOT,   SFT_T(KC_SLSH)},
-{GUI_T(KC_TAB), MO(FKEY), MO(NUMS), LOWERKEY, CTL_T(KC_ESC), ALT_T(KC_BSPC), KC_ENT,  KC_SPC, RAISEKEY, MO(CURS), TG(CURS), KC_LGUI}
+{KC_Q,          KC_W,     KC_E,     KC_R,     KC_T,          KC_LBRC, KC_RBRC, KC_Y,   KC_U,     KC_I,     KC_O,     KC_P},
+{KC_A,          KC_S,     KC_D,     KC_F,     KC_G,          KC_LPRN, KC_RPRN, KC_H,   KC_J,     KC_K,     KC_L,     SEMIKEY},
+{SFT_T(KC_Z),   KC_X,     KC_C,     KC_V,     KC_B,          KC_LCBR, KC_RCBR, KC_N,   KC_M,     KC_COMM,  KC_DOT,   SFT_T(KC_SLSH)},
+{GUI_T(KC_TAB), MO(FKEY), MO(NUMS), LOWERKEY, CTL_T(KC_ESC), KC_BSPC, KC_ENT,  KC_SPC, RAISEKEY, MO(CURS), TG(CURS), KC_LALT}
 },
 
   
